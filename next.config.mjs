948ed65a696612142bbf625df71485ca161233f9
sentinelstechnologygroup-stack <sentinelstechnologygroup@gmail.com>
2026-03-17 22:30:04 -0500

    Hide unfinished pages with redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/work",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/insights",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
